2b3c2034b18f755b6e728a47ebfd334c216514f8
asfalfa <glkin9@gmail.com>
2023-10-06 17:48:23 +0200

    feat(FE): :sparkles: Add/Remove/Move images from cover to gallery and viceversa

--- src/views/admin/posts/EditPostSingleView.vue
+++ src/views/admin/posts/EditPostSingleView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { editPost } from '../../../services/posts'
 import { useRoute } from 'vue-router';
-import { getPostData } from '../../../services/posts';
+import { getPostData , removePostImage, removePost, movePostImage } from '../../../services/posts';
 import {
     onMounted,
     ref
@@ -21,29 +21,100 @@ async function getPost() {
 }
 
 let success = ref(false);
+let removeSuccess = ref(false);
+let moveSuccess = ref(false);
 let failure = ref(false);
 let loading = ref(false); 
 let contentRules = [value => checkContent(value)];
 let timeout = null;
+let covers = ref(null);
+let gallery = ref(null);
+
+function uploadGallery (event) {
+  gallery.value = event.target.files
+}
+function uploadCovers (event) {
+  covers.value = event.target.files
+}
+
+async function moveImageTo(image, type){
+  try{
+    const res = await movePostImage(image, type, route.params.id);    
+    if (res.data.valid == true) {
+      moveSuccess.value = true;
+    }
+  } catch (err){
+      if(err.response){
+        console.log("Server Error:", err);
+      }
+  }
+}
+
+
+async function removeImage(image, type){
+  try{
+    const res = await removePostImage(image, type, route.params.id);
+    if (res.data.valid == true) {
+      removeSuccess.value = true;
+    }
+  } catch (err){
+      if(err.response){
+        console.log("Server Error:", err);
+      }
+  }
+}
+
+async function remove(){
+  try{
+    const res = await removePost(route.params.id);
+    if (res.data.valid == false) {
+      failure.value = true;
+    } else{
+      failure.value = false;
+      success.value = true;
+      setTimeout(() => {
+        const url = new URL('/admin/posts/edit', window.location.origin);
+        window.location.href = url.toString();
+        // router.push({ path: '/admin/posts/edit', prop: { plan: 'private' } })
+      }, 5000)
+    }
+  } catch (err){
+      if(err.response){
+        console.log("Server Error:", err);
+      }
+  }
+}
 
 async function submit() {
     loading.value = true;
 
-    let data = {
-      id: route.params.id,
-      title: post.value.title,
-      content: post.value.content,
-      author_name: post.value.author_name,
-      top_image_url: post.value.top_image_url,
-      bottom_image_url: post.value.bottom_image_url,
+    const formData = new FormData();
+    //Check if this works
+    //Add functions to remove gallery and cover images
+    if(gallery.value != null){
+      for (const i of Object.keys(gallery.value)) {
+        formData.append('gallery', gallery.value[i])
+      };
     }
-    editPost(data).then(res => {
+    
+    if(covers.value != null){
+      for (const i of Object.keys(covers.value)) {
+        formData.append('covers', covers.value[i])
+      };
+    }
+
+    formData.append('id', route.params.id);
+    formData.append('title', post.value.title);
+    formData.append('content', post.value.content);
+    formData.append('category', post.value.category);
+    formData.append('author_name', post.value.author_name);
+    console.log(formData)
+    editPost(formData).then(res => {
       if (res.data.valid == false) {
         failure.value = true;
       } else{
         failure.value = false;
         success.value = true;
-        console.log(success.value)
         setTimeout(() => {
           const url = new URL('/admin/posts/edit', window.location.origin);
           window.location.href = url.toString();
@@ -81,6 +152,13 @@ onMounted(() => {
         required
       ></v-text-field>
   
+      <v-text-field
+        v-model="post.category"
+        :rules="contentRules"
+        label="Title"
+        required
+      ></v-text-field>
+
       <v-textarea
         clearable
         v-model="post.content"
@@ -96,28 +174,46 @@ onMounted(() => {
         required
       ></v-text-field>
 
-      <v-text-field
-        v-model="post.top_image_url"
-        :rules="contentRules"
-        label="Top Image"
-        required
-      ></v-text-field>
+      <label>Cover Images</label>
+      <div class="grid grid-cols-6 mt-1 mb-5">
+        <div v-for="image in post.covers" class="mr-2">
+          <v-img :src="image"></v-img>
+          <v-btn @click="moveImageTo(image, 'covers')" block rounded="0">To Gallery</v-btn>
+          <v-btn @click="removeImage(image, 'covers')" block rounded="0">Remove</v-btn>
+        </div>
+        <div class="m-auto flex flex-col">
+          <label>Add New Cover Images</label>
+          <input type="file" @change="uploadCovers" multiple />
+        </div>
+      </div>
 
-      <v-text-field
-        v-model="post.bottom_image_url"
-        :rules="contentRules"
-        label="Bottom Image"
-        required
-      ></v-text-field>
+      <label>Gallery Images</label>
+      <div class="grid grid-cols-6 mt-1">
+        <div v-for="image in post.gallery" class="mr-2">
+          <v-img :src="image"></v-img>
+          <v-btn @click="moveImageTo(image, 'gallery')" block rounded="0">To Cover</v-btn>
+          <v-btn @click="removeImage(image, 'gallery')" block rounded="0">Remove</v-btn>
+        </div>
+        <div class="m-auto flex flex-col">
+          <label>Add New Gallery Images</label>
+          <input type="file" @change="uploadGallery" multiple />
+        </div>
+      </div>
 
       <v-btn
         :loading="loading"
         type="submit"
         block
+        rounded="0" 
         class="mt-2"
         text="Submit"
       ></v-btn>
     </v-form>
+    <v-btn @click="remove()" color="red" rounded="0" class="mt-5">Remove Post</v-btn>
+    
+    <v-card v-if="removeSuccess">
+      Image deleted.
+    </v-card>
 
     <v-card v-if="success">
       <v-card-title class="w-min mx-auto">Post edited.</v-card-title>
