2c5ed4a3139bb9923347293f287176de5b229f6e
asfalfa <glkin9@gmail.com>
2023-10-06 17:45:58 +0200

    feat(FE): :sparkles: Add image gallery to post

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -6,7 +6,6 @@ import {
     ref,
     computed
 } from 'vue'
-
 const posts = ref([]);
 
 function getPosts() {
@@ -29,13 +28,16 @@ onMounted(() => {
     <v-container class="grid grid-cols-2 gap-4">
         <v-card v-for="post in posts" :key=post.title className='mx-auto bg-neutral-200 hover:bg-white border-solid border-2 border-white hover:border-black w-3/4 hover:saturate-150'>
             <RouterLink :to="'/posts/' + post.id" :key=post.title>
-                <div className='overflow-hidden grid content-center'>
+                <div v-if="post.covers && post.covers[0]" className='overflow-hidden grid content-center'>
                     <img className='h-[200px] md:h-[300px] align-center w-full' :src=post.covers[0] />
                 </div>
                 <div className='p-2 text-center'>
                     <div>
                         <small>
-                            <Date :date="post.date" /> | Category
+                            <Date :date="post.date" />
+                            <div>
+                                <RouterLink class="mx-[2px] px-[8px]" v-for="cat in post.category.slice(0, 4)" :to="'/tags/' + cat" :key=cat>{{cat.split('-').join(' ')}}</RouterLink>
+                            </div>
                         </small>
                     </div>
                     <div className='font-semibold text-xl'>{{post.title}}</div>
@@ -45,7 +47,8 @@ onMounted(() => {
                         </small>
                     </div>
                 </div>
-                <div className='overflow-hidden grid content-center'>
+                <div v-if="post.covers && post.covers[1]" className='overflow-hidden grid content-center'>
+                    <!-- make the images clickable and expandable -->
                     <img className='h-[200px] md:h-[300px] align-center w-full' :src=post.covers[1] />
                 </div>
             </RouterLink>
--- src/components/single-post/SinglePost.vue
+++ src/components/single-post/SinglePost.vue
@@ -9,6 +9,7 @@ import {
 
 const route = useRoute();
 const post = ref({});
+const selectedImage = ref(null);
 
 async function getPost() {
     try {
@@ -21,6 +22,25 @@ async function getPost() {
     }
 }
 
+function selectImage(img){
+    if (img == null) {
+        selectedImage.value = null;
+    } else{
+        selectedImage.value = img;
+    }
+}
+
+function browseImage(img, dir){
+    const index = (post.value.gallery).findIndex((el) => el == img);
+
+    if (dir == 0 && index != 0) {
+        selectedImage.value = post.value.gallery[index - 1];
+    } else if (dir == 1 && index != post.value.gallery.length -1){
+        selectedImage.value = post.value.gallery[index + 1];
+    }
+}
+
+
 onMounted(() => {
     getPost();
 });
@@ -29,13 +49,16 @@ onMounted(() => {
 <template>
     <v-container class="px-2">
         <v-card className='mx-auto bg-neutral-200 hover:bg-white border-solid border-2 border-white w-full md:w-5/6'>
-            <div className='overflow-hidden grid content-center'>
-                <v-img v-if="post.covers" className='align-center w-full' :src="post.covers[0]"></v-img>
+            <div v-if="post.covers && post.covers[0]" className='overflow-hidden grid content-center'>
+                <v-img className='align-center w-full' :src="post.covers[0]"></v-img>
             </div>
-            <div className='mx-2 p-2 text-center text-black/90 border-b-[1px] border-black'>
+            <div className='mx-2 p-2 text-center text-black/90 border-b-[1px] border-black/50'>
                 <div>
                     <small>
-                        <Date v-if="post.date" :date="post.date" /> | Category
+                        <Date v-if="post.date" :date="post.date" />
+                        <div>
+                            <RouterLink class="mx-[2px] px-[8px]" v-for="cat in post.category" :to="'/tags/' + cat" :key=cat>{{cat}}</RouterLink>
+                        </div>
                     </small>
                 </div>
                 <div className='font-semibold text-xl'>{{post.title}}</div>
@@ -48,9 +71,31 @@ onMounted(() => {
             <div class="text-black/90 px-8 py-4 text-justify">
                 {{ post.content }}
             </div>
-            <div class="grid grid-cols-4">
-                <v-img v-for="image in post.gallery" :src="image"></v-img>
+            <div class="grid grid-cols-4 py-4">
+                <v-img class="h-[250px] cursor-pointer" v-for="image in post.gallery" :src="image" @click="selectImage(image)"></v-img>
             </div>
         </v-card >
     </v-container>
+    <v-overlay class="my-overlay bg-black/50" v-model="selectedImage">
+        <v-btn @click="browseImage(selectedImage, 0)">Before</v-btn>
+        <v-img
+            @click="selectImage(null)"
+            :src="selectedImage"
+            >
+        </v-img>
+        <v-btn @click="browseImage(selectedImage, 1)">After</v-btn>
+    </v-overlay>
 </template>
+
+<style scoped>
+.my-overlay{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.my-overlay:deep(.v-overlay__content) {
+    width: 80%;
+    height: 80%;
+    margin: auto;
+  }
+</style>
